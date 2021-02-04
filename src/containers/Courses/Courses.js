fdb0610abc609464f73503458828ffd8c09090c4
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-04 17:50:33 -0300

    created routes for users and courses

--- src/containers/Courses/Courses.js
+++ src/containers/Courses/Courses.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 import './Courses.css';
 
@@ -18,7 +19,11 @@ class Courses extends Component {
                 <section className="Courses">
                     {
                         this.state.courses.map( course => {
-                            return <article className="Course" key={course.id}>{course.title}</article>;
+                            return (
+                                <Link to="/course" key={course.id}>
+                                    <article className="Course" >{course.title}</article>
+                                </Link>
+                            );
                         } )
                     }
                 </section>
