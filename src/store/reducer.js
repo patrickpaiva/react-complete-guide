876e287823687bd046786e55ee471d471316f971
Patrick Paiva <patrickpaiva88@gmail.com>
2021-02-08 12:00:08 -0300

    added input to name and age with normal states and connected then to redux state

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -7,8 +7,8 @@ const reducer = (state = initialState, action) => {
         case 'ADD_PERSON':
             const newPerson = {
                 id: Math.random(), // not really unique but good enough here!
-                name: 'Max',
-                age: Math.floor( Math.random() * 40 )
+                name: action.name,
+                age: action.age
             }
             const newPersonsArr = state.persons.concat(newPerson)
             return {
